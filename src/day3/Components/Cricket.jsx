f46c94b6bd2062ba4721b8466f06c9dbc9121473
Srinivasahan <srinivasayyappan13@gmail.com>
2023-11-08 12:37:43 +0530

    new cricket

--- src/day3/Components/Cricket.jsx
+++ src/day3/Components/Cricket.jsx
@@ -5,13 +5,15 @@ export const Cricket = () => {
     const [wicket,updateWicket]=useState(0);
   return (
     <div>
-        <p>Runs:${curr}</p>
-        <p>Wicket:${wicket}</p>
+        <p>Runs:{curr}</p>
+        <p>Wicket:{wicket}</p>
         <button onClick={()=>update(curr+1)}>1 runs</button>
         <button onClick={()=>update(curr+2)}>2 runs</button>
         <button onClick={()=>update(curr+4)}>4 runs</button>
         <button onClick={()=>update(curr+6)}>6 runs</button>
+        <button onClick={()=>update(0)}>Reset Runs</button>
         <button onClick={()=>updateWicket(wicket+1)}>Wicket</button>
+        <button onClick={()=>updateWicket(0)}>Reset Wickets</button>
     </div>
   )
 }
