1fe73f951af21e40e3864171c357ca679d98d88d
Srinivasahan <srinivasayyappan13@gmail.com>
2023-11-08 08:05:51 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Footer= () =>{
+    return(
+        <div>
+           
+           <hr></hr>
+           <h2 style={{backgroundColor:"Skyblue", fontFamily:"cursive"}}><center><b>This is my First React App</b></center></h2>
+           <hr></hr>
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Main= () =>{
+    return(
+      <div><center>
+      <label for="name">Email Id: &emsp;</label>
+              <input  id="name" type="box" placeholder="Enter Email ID"></input>
+           <br>
+           </br>
+               <label for="na">Password: </label>
+             <input id="na" type="box" placeholder="Enter Password"></input>
+           <br>
+           </br>
+             
+            <br>
+            </br>
+        
+            <button variant="outlined" color="blue">Submit</button>
+      </center>
+        </div>
+    );
+};
+
+export default Main;
