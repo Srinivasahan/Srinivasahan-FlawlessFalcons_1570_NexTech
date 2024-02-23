d4205575f1a3fc237129fe2922d3e8a5eb6b3706
Srinivasahan <srinivasayyappan13@gmail.com>
2024-02-23 14:05:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Button from "./day3/Components/Button";
 import { Cricket } from "./day3/Components/Cricket";
 import { Dynamicrendering } from "./day3/Components/Dynamicrendering";
+import { Goku } from "./day3/Components/Goku";
+import Toggle from "./day3/Components/Toggle";
 function App() {
   return (
     <div className="flex space-x-10 my-8">
@@ -11,6 +13,8 @@ function App() {
       <Button color="blue">Blue Button</Button>
       <Dynamicrendering></Dynamicrendering>
       <Cricket></Cricket>
+      <Goku></Goku>
+      <Toggle></Toggle>
     </div>
   );
 }
